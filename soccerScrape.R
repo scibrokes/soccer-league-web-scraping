5ba384331f64d0e7b5882988d780b794da6b2eca
Eng Lian Hu <englianhu@gmail.com>
2014-12-23 22:32:05 +0800

    update
    scrape and save database

--- soccerScrape.R
+++ soccerScrape.R
@@ -2,5 +2,5 @@
 source('C:/Users/Scibrokes Trading/Documents/GitHub/englianhu/Soccer-League-Web-Scraping/function/downloadMatch.R')
 URL = "http://app.en.gooooal.com/soccer/statistic/standing.do?lid=4"
 eng2012 = downloadMatch(URL, year = 2012)
-eng2013 = downloadMatch(URL, year = 2013)
+eng2013 = downloadMatch(URL, year = 2013, timeout=30)
 
