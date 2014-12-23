b9d2db4bc8de3c3c6e83e58fb2acdbb056e38f3f
Eng Lian Hu <englianhu@gmail.com>
2014-12-23 20:56:35 +0800

    update

--- soccerScrape.R
+++ soccerScrape.R
@@ -1,5 +1,5 @@
 # Simulate to scrape English soccer matches (Static webpage, we need RSelenium package for dynamic webpage)
-source('https://github.com/englianhu/Soccer-League-Web-Scraping/blob/master/function/downloadMatch.R')
+source('C:/Users/Scibrokes Trading/Documents/GitHub/englianhu/Soccer-League-Web-Scraping/function/downloadMatch.R')
 URL = "http://app.en.gooooal.com/soccer/statistic/standing.do?lid=4"
 eng2012 = downloadMatch(URL, year = 2012)
 eng2013 = downloadMatch(URL, year = 2013)
